3014d141c0d9433a5d5c1e2c5dd2857aa6082038
K-June <80251711+Lee-June-Kyu@users.noreply.github.com>
2021-05-11 22:12:30 +0900

    Add files via upload

---
+++ svelte/src/components/Logo.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { link } from 'svelte-spa-router'
+</script>
+
+<a
+  use:link
+  href="/">
+  <span>Findrama</span>
+</a>
+
+<style lang="scss">
+  a {
+    display: block;
+    font-family: 'Oswald', sans-serif;
+    font-size: 20px;
+    color: $color--white-50;
+    text-decoration: none;
+    span {
+      color: $color--primary;
+    }
+  }
+</style>
