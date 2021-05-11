d45df585c0d5ad95bd74d2e07470bda3862e7c7c
hyogeun-byun <withwidin@naver.com>
2021-05-12 01:05:35 +0900

    add OSS folder 변효근

---
+++ OSS/static/Footer.svelte
@@ -0,0 +1,27 @@
+<footer>
+  <div>
+    <a>
+     Findrama
+    </a>
+  </div>
+  <div>
+    <a>
+      @{new Date().getFullYear()}
+      OSS
+    </a>
+  </div>
+</footer>
+
+<style lang="scss">
+  footer {
+    padding: 70px;
+    text-align: center;
+    a {
+      color: $color--white-10;
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+</style>
---
+++ OSS/static/Headline.svelte
@@ -0,0 +1,35 @@
+<div class="headline">
+  <h1>
+    <span>Findrama</span><br />
+    드라마 검색<br />
+    드라마 평가
+  </h1>
+  <div class="description">
+    <p>
+      드라마 검색을 위한 Search페이지와 드라마 평가를 위한 Drama페이지로 이루어져있다.
+    </p>
+    <p>
+      드라마는 장르, 방영연도, 이름으로 검색이 가능하다.
+    </p>
+  </div>
+</div>
+
+<style lang="scss">
+  .headline {
+    h1 {
+      line-height: 1;
+      font-family: 'Oswald', sans-serif;
+      font-size: 80px;
+      @media #{$mobile} {
+        font-size: 60px;
+      }
+      span {
+        color: $color--primary;
+      }
+    }
+    .description {
+      margin: 30px 0;
+      color: $color--white-30;
+    }
+  }
+</style>
