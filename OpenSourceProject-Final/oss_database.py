d6bdf322be1e82db1c98eab08c9b27cbd9c6f5db
jungeun-dev <69452161+jungeun-dev@users.noreply.github.com>
2021-06-02 13:50:15 +0900

    Update oss_database.py
    Add comment

--- OpenSourceProject-Final/oss_database.py
+++ OpenSourceProject-Final/oss_database.py
@@ -9,15 +9,15 @@ class Database():
                                   charset='utf8')
         self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
 
-    def execute(self, query, args={}):
+    def execute(self, query, args={}):                          # sql문 실행
         self.cursor.execute(query, args)
 
-    def executeOne(self, query, args={}):
+    def executeOne(self, query, args={}):                       # 하나의 tuple fectch
         self.cursor.execute(query, args)
         row = self.cursor.fetchone()
         return row
 
-    def executeAll(self, query, args={}):
+    def executeAll(self, query, args={}):                       # 전체 tuple fectch
         self.cursor.execute(query, args)
         row = self.cursor.fetchall()
         return row
