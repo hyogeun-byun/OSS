9c6c8d6d1639b40166e57260ebee4204fb21c308
jungeun-dev <69452161+jungeun-dev@users.noreply.github.com>
2021-06-02 14:12:19 +0900

    Update crawling_talks.py 이정은
    Add comment

--- crawling_dramatalks/drama_talks.py
+++ crawling_dramatalks/drama_talks.py
@@ -4,14 +4,16 @@ from bs4 import BeautifulSoup
 import urllib
 import pymysql
 
+# 드라마 댓글 크롤링, DB저장
 def search_talks_into_db(index,drama_chanel, drama_name):
-    count = 0
+    count = 1
     # Open database connection
     db = pymysql.connect(host='localhost', port=3306, user='findrama', passwd='findrama', db='findrama', charset='utf8',
                          autocommit=True)
     # prepare a cursor object using cursor() method
-    cursor = db.cursor()
-
+    cursor = db.cursor()        #DB 접근
+    
+    #chrome driver 사용
     driver = webdriver.Chrome(r'C:\Users\paqgl\Downloads\chromedriver.exe')
     url = 'https://tv.naver.com/'+drama_chanel+'.'+ drama_name +'/talks'
     driver.get(url)
@@ -26,27 +28,27 @@ def search_talks_into_db(index,drama_chanel, drama_name):
             soup = BeautifulSoup(text, 'html.parser')
 
             for li in soup.select('div.u_cbox_area'):
-                nickname = li.select_one('span.u_cbox_nick').text
+                nickname = li.select_one('span.u_cbox_nick').text   # 댓글 작성자 아이디 크롤링
                 try:
-                    talk = li.select_one('span.u_cbox_contents').text
+                    talk = li.select_one('span.u_cbox_contents').text  
                 except:
                     continue
 
                 # execute SQL query using execute() method.
                 sql = "insert into drama_talks values ("+str(index) + ",'" + nickname + "'" + "," + "'" + talk + "'" + ")"
-                print(count ,sql)
+                #print(count ,sql)
 
                 try:
                     cursor.execute(sql)
-                except:
+                except:                                # 클린 로봇이 처리한 댓글은 가져오지 않음
                     continue
-                print(nickname, ' :  ', talk)
+                #print(nickname, ' :  ', talk)
             select = '#cbox_module_talk > div > div.u_cbox_paginate > div > a:nth-child(' + str(idx) + ')'
             try:
-                button = driver.find_elements_by_css_selector(select)[0]
+                button = driver.find_elements_by_css_selector(select)[0]  # 페이지 2,3,4,5,다음페이지 버튼 클릭
             except:
                 break
             button.click()
             idx += 1
             time.sleep(3)
-            count += 1
+        count += 1
